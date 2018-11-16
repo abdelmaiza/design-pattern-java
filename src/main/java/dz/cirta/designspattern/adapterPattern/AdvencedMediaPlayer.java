695be1ff91d207d4773b0fb91658e87ad2cc6c83
Abdeldjallil MAIZA <AMAIZA@externe.generali.fr>
2018-11-16 14:52:59 +0100

    no message

---
+++ src/main/java/dz/cirta/designspattern/adapterPattern/AdvencedMediaPlayer.java
@@ -0,0 +1,9 @@
+package dz.cirta.designspattern.adapterPattern;
+
+/**
+ * Created by b010nsm on 13/11/2018.
+ */
+public interface AdvencedMediaPlayer {
+    public void playVlc (String fileName);
+    public void playMp4 (String fileName);
+}
---
+++ src/main/java/dz/cirta/designspattern/adapterPattern/MediaPlayer.java
@@ -0,0 +1,8 @@
+package dz.cirta.designspattern.adapterPattern;
+
+/**
+ * Created by b010nsm on 13/11/2018.
+ */
+public interface MediaPlayer {
+    public void play (String audioType, String fileName);
+}
