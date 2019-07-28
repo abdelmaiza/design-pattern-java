d339dee0cabbdeba3727f75a1505402633be87cc
amaiza <abdeldjallil.maiza@gmail.com>
2019-07-28 20:47:09 +0200

    creation builder pattren

---
+++ src/main/java/dz/cirta/designspattern/builderPattern/BuilderDemo.java
@@ -0,0 +1,18 @@
+package dz.cirta.designspattern.builderPattern;
+
+public class BuilderDemo {
+    public static void main (String [] args){
+        LunchOrder order = new LunchOrder.LunchBuilder()
+                .bread("Wheat")
+                .condiments("Lotuce")
+                .dressing("Mayo")
+                .meat("Turkey")
+                .build();
+
+        System.out.println(order.getBread());
+        System.out.println(order.getCondiments());
+        System.out.println(order.getDressing());
+        System.out.println(order.getMeat());
+    }
+
+}
